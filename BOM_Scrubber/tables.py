2a1ebf2c754eb4129d81a6f4ec1f925c6e6d0ee9
John Shebey <jshebey@vt.edu>
2023-12-18 13:45:38 -0500

    Added KEMET tables/datasheet

---
+++ BOM_Scrubber/tables.py
@@ -0,0 +1,27 @@
+#KEMET Capacitor decoding tables
+kemetToleranceTable = [
+  ['B', '± 0.10 pF'],
+  ['C', '± 0.25 pF'],
+  ['D', '± 0.5 pF'],
+  ['F', '± 1 %'],
+  ['G', '± 2 %'],
+  ['J', '± 5 %'],
+  ['K', '± 10 %'],
+  ['M', '± 20 %']
+]
+
+kemetVoltageTable = [
+  ['8', '10 V'],
+  ['4', '16 V'],
+  ['3', '25 V'],
+  ['5', '50 V'],
+  ['1', '100 V'],
+  ['2', '200 V'],
+  ['A', '250 V']
+]
+
+kemetDielectricTable = [
+  ['G', 'NPO'],
+  ['R', 'X7R'],
+  ['P', 'X5R']
+]
