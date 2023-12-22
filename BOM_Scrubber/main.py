5a4d5ec0d2c21555615e0fb282f9984d83186473
John Shebey <jshebey@vt.edu>
2023-12-22 12:09:10 -0500

    Set up Pandas library

--- BOM_Scrubber/main.py
+++ BOM_Scrubber/main.py
@@ -1,11 +1,11 @@
 from scrub import Scrub
+import pandas as pd
 
 def main():
     bom_file = "path/to/your/bom_file.txt"
     bom_processor = Scrub(bom_file)
     bom_processor.process_bom()
     bom_processor.display_parts_info()
-    #git test
 
 if __name__ == "__main__":
     main()
