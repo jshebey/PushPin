a128f4239892128d436d9e666a69e9acbef7720e
John Shebey <jshebey@vt.edu>
2023-12-24 01:37:21 -0500

    Wrote the inputs for each manufacturer/family for the decode method

--- BOM_Scrubber/part.py
+++ BOM_Scrubber/part.py
@@ -1,3 +1,5 @@
+from tables import *
+
 class Part:
     def __init__(self, part_number):
         self.part_number = part_number
@@ -14,19 +16,55 @@ class Part:
     def find_res_data(self):
         if(self.part_number[0:5] == 'RK73H'):
             self.manufacturer = 'KOA (RK73H series)'
-            self.data = []
+            #[size string, size table, convert, tolerance string, tolerance table, convert, power string, power table, convert, voltage string, voltage table, convert, resistance string]
+            self.data = [
+                self.partNumber[5:7], koaSizeTable, 1,          #size data 
+                self.partNumber[15],  koaToleranceTable, 1,     #tolerance data
+                self.partNumber[5:7], koaPowerTable, 1,         #power data
+                self.partNumber[5:7], koaVoltageTable, 1,       #voltage data
+                self.partNumber[11:15]                          #resistance
+            ]
+
         elif(self.part_number[0:3] == 'FRC'):
             self.manufacturer = 'Kamaya (FRC series)'
-            self.data = []
+            self.data = [
+                self.partNumber[3:5], kamayaSizeTable, 1,          #size data 
+                self.partNumber[11],  kamayaToleranceTable, 1,     #tolerance data
+                self.partNumber[3:5], kamayaPowerTable, 1,         #power data
+                self.partNumber[3:5], kamayaVoltageTable, 1,       #voltage data
+                self.partNumber[8:11]                              #resistance
+            ]
+
         elif(self.part_number[0:2] == 'RR'):
             self.manufacturer = 'Susumu (RR series)'
-            self.data = []
+            self.data = [
+                self.partNumber[2:6], susumuSizeTable, 1,          #size data 
+                self.partNumber[12],  susumuToleranceTable, 1,     #tolerance data
+                self.partNumber[2:6], susumuPowerTable, 1,         #power data
+                self.partNumber[2:6], susumuVoltageTable, 1,       #voltage data
+                self.partNumber[8:11]                              #resistance
+            ]
+
         elif(self.part_number[0:2] == 'RC'):
             self.manufacturer = 'YAGEO (RC_L series)'
-            self.data = []
+            self.data = [
+                self.partNumber[2:6], 'N/A', 0,                    #size data 
+                self.partNumber[6],  yageoToleranceTable, 1,       #tolerance data
+                self.partNumber[2:6], yageoPowerTable, 1,          #power data
+                self.partNumber[2:6], yageoVoltageTable, 1,        #voltage data
+                self.partNumber[11:15]                             #resistance
+            ]
+
         elif(self.part_number[0:3] == 'CRM'):
             self.manufacturer = 'Bourns (CRM series)'
-            self.data = []
+            self.data = [
+                self.partNumber[3:7], 'N/A', 0,                    #size data 
+                self.partNumber[8],  bournsToleranceTable, 1,      #tolerance data
+                self.partNumber[3:7], bournsPowerTable, 1,         #power data
+                self.partNumber[3:7], bournsVoltageTable, 1,       #voltage data
+                self.partNumber[11:15]                             #resistance
+            ]
+
         else:
             pass
 
